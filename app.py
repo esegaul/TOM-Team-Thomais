a8667f9de6a2cb0a19a0133c8b60ee1d62c11926
Theriley106 <christopherlambert106@gmail.com>
2019-01-19 10:30:10 -0500

    Added dynamic guide names

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 app = Flask(__name__, static_url_path='/static')
 
 DB_FILE = "database.json"
+HTML_TEMPLATE = "template.html"
 
 if os.path.exists(DB_FILE) == False:
 	os.system("echo [] >  {}".format(DB_FILE))
@@ -21,24 +22,27 @@ def add_problem_to_file(data):
 		with open(DB_FILE, 'w') as outfile:
 			json.dump(a, outfile)
 
+def get_problem_type_info(problemType):
+	return {'problemType': problemType}
+
 @app.route('/', methods=['GET'])
 def index():
 	return render_template("index.html")
 
 
 @app.route('/addProblem', methods=['POST'])
-def addProblem():
+def add_problem():
 	problem = request.form.get('problem', None)
 	print problem
 	add_problem_to_file(problem)
 	# Adds the inputted problem to a file
 	return ('', 204)
 
-@app.route('/getProblems', methods=['GET'])
-def getProblems():
-	a = json.load(open(DB_FILE))
-	return jsonify(sorted(set(a), key=a.index))
-
+@app.route('/guide/<problemType>', methods=['POST'])
+def get_guide(problemType):
+	if os.path.exists("templates/{}.html".format(problemType)):
+		os.system("cp {} {}.html".format(HTML_TEMPLATE, problemType))
+	return render_template("{}.html".format(problemType), info=get_problem_type_info(problemType))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
